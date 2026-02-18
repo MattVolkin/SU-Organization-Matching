06898e42b15df90f2a7924f6efbdbbd9ac959078
Ben McKallip <mckallipb@southwestern.edu>
2026-02-18 11:35:53 -0600

    updated for static rendering using `npm run build`

--- Svelte Examples/plain-svelte-app/svelte.config.js
+++ Svelte Examples/plain-svelte-app/svelte.config.js
@@ -1,8 +1,18 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
+};
+
+export default config;
