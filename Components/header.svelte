465291b6bf367d513d84a8847d70a1d7bff00aaa
Matthew Volkin <119533382+MattVolkin@users.noreply.github.com>
2026-02-25 11:38:25 -0600

    Create header.svelte
    issue #28

---
+++ Components/header.svelte
@@ -0,0 +1,19 @@
+<script>
+export let userType = $state("");
+</script>
+
+<header>
+  <h1>PLACEHOLDER	<!-- TODO: Replace with actual title --></h1>
+  <nav>
+    <a href="/">Home</a>
+    <a href="/about">About this Project</a>
+    <a href="/howto">How To Use this tool</a>
+    {#if userType === 'admin'}
+      <a href="/create">Create New Club</a>
+      <a href="/change-user-type">Change User Type</a> <!-- TODO: Implement change user type functionality -->
+    {:else if userType === 'officer'}
+      <a href="/manage-club">Manage Club</a> <!-- TODO: Implement manage club functionality -->>
+    {/if}
+    <!-- Add more links as needed -->
+  </nav>
+</header>
