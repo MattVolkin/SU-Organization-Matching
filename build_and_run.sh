490fa63c507fb2393f6d15a7204dda6b492e6b8b
Ben McKallip <mckallipb@southwestern.edu>
2026-03-05 19:18:24 +0000

    corrected server log file path

--- build_and_run.sh
+++ build_and_run.sh
@@ -2,4 +2,4 @@
 pkill -f '\./server' || true
 
 cd 'Svelte Examples/plain-svelte-app' && npm run build
-cd '../../Server Examples' && go mod tidy && go build server.go && chmod +x server && ./server > /server.log 2>&1 &
+cd '../../Server Examples' && go mod tidy && go build server.go && chmod +x server && ./server > server.log 2>&1 &
