e84257f1364ee4f401d7c506111d9022596b716e
Matt Volkin <volkinm@southwestern.edu>
2026-03-08 15:38:39 +0000

    worked on getting info from the server #35

--- Svelte Examples/plain-svelte-app/src/App.svelte
+++ Svelte Examples/plain-svelte-app/src/App.svelte
@@ -1,96 +1,36 @@
 <script>
-
+  import{ onMount } from 'svelte';
   import Header from '../../../Components/header.svelte'
   import Footer from '../../../Components/footer.svelte'
-  //setup for the questions
-  let genderoptions = ["Man", "Woman", "Non-Binary", "Other", "Prefer not to say"]
-  let raceoptions = ["American Native/Alaska Native", "Asian", "Black or African American", "Hispanic or Latino", "Middle Eastern or North African", "Native Hawaiian or Pacific Islander", "White", "Prefer not to say"]
-  let religionoptions = ["Protestantism", "Catholocism", "Judaism", "Islam", "Buddhism", "Hinduism", "No religion", "Other", "Prefer not to say"]
-  let majoroptions = ["Anthropology", "Applied Physics", "Art (Studio)", "Art History", "Biochemistry", "Biology", "Business", "Chemistry", "Classics", "Communication Studies", "Computational Mathematics", "Computer Science", "Pre-Dentistry", "Economics", "Education", "Pre-Engineering", "English", "Environmental Studies", "Feminist Studies", "French", "German", "Greek", "Health Professions", "History", "International Studies", "Kinesiology", "Latin", "Latin American & Border Studies", "Pre-Law", "Mathematics", "Pre-Med", "Pre-Ministry", "Music", "Pre-Nursing", "Pre-Occupational Therapy", "Philosophy", "Physics", "Political Science", "Psychology", "Pre-Physician Assistant", "Pre-Physical Therapy", "Religion", "Sociology", "Spanish", "Theatre", "Undecided"]
-  //feilds in the form
-  let name = ''
-  let club = ''
-  let contact = ''
-  let officer = ""
-  let email = ''
-  let gender = ''
-  let race = ''
-  let religion = ''
-  let major = ''
-/**@function submit - Handles form submission */
-  function submit() {
-    console.log(`Name: ${name}, Club: ${club}, Contact: ${contact}, Officer : ${officer}, Email: ${email}, Gender: ${gender}, Race: ${race}, Religion: ${religion}, Major: ${major}`)//for debuging logs the form data
-    fetch('http://localhost:8080/submit', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ name, club, contact, officer, email, gender, race, religion, major })//sends the form data to the backend server as a JSON object
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Success:', data)
-    })
-    .catch((error) => {
-      console.error('Error:', error)
-    })
+  let results = []
+  let pageNum = $state(1) // for keeping track of what page of results the user is on. Not currently being used but will be helpful for future implementation 
+
+  async function getResults() {
+    const response = await fetch('http://localhost:8080/results')
+    results = await response.json() // in backend please make the list of results an array of clubs (sorted in decending order of match percentage) with each club having a name, description and match percentage field
   }
+
+  async function nextPage() {
+    pageNum += 1
+    await getResults() // for future implementation when we have more results than we want to show on one page, this will fetch the next page of results from the backend
+  }
+
+  async function prevPage() {
+    if (pageNum > 1) {
+      pageNum -= 1
+      await getResults() // for future implementation when we have more results than we want to show on one page, this will fetch the previous page of results from the backend
+    }
+  }
+  
+  async function getClubInfo(club) {
+
+    
+  }
+    
 </script>
-
+  
 <Header />
-<main>
-  <h1>Demographic Form</h1>
-
-  <form on:submit|preventDefault={submit}>
-    <label for="name">Name:</label>
-    <input id="name" type="text" bind:value={name} required /> <!-- Text input for the user's name, bound to the 'name' variable -->
-
-    <fieldset>
-      <legend>Gender</legend>
-      {#each genderoptions as option} <!-- Loop through gender options to create radio buttons -->
-        <label>
-          <input type="radio" name="gender" value={option} bind:group={gender} required />
-          {option}
-        </label>
-      {/each}
-    </fieldset>
-
-    <fieldset>
-      <legend>Race/Ethnicity (Select all that apply)</legend> <!-- Fieldset for race/ethnicity options -->
-      {#each raceoptions as option} <!-- Loop through race options to create checkboxes -->
-        <label>
-          <input type="checkbox" name="race" value={option} bind:group={race} required />     
-          {option}
-        </label>
-      {/each}
-    </fieldset>
-
-    <fieldset>
-      <legend>Intended Major(s)/Program of Study</legend>
-      <select multiple bind:value={major} required>
-        <option value="">Select a major...</option>
-        {#each majoroptions as option} <!-- Loop through major options to create dropdown options -->
-          <option value={option}>{option}</option>
-        {/each}
-      </select>
-    </fieldset>
-
-    <fieldset>
-      <legend>Religion</legend>
-      {#each religionoptions as option} <!-- Loop through religion options to create radio buttons -->
-        <label>
-          <input type="radio" name="religion" value={option} bind:group={religion} required />
-          {option}
-        </label>
-      {/each}
-    </fieldset>
-
-
-
-
-    <button type="submit">Submit</button>
-  </form>
-</main>
+  <h1> Results</h1>
 <Footer />
 <style>
   main {
