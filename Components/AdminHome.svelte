0da287b50d6a2f6429c7317f4deae7f614b2b975
Matt Volkin <volkinm@southwestern.edu>
2026-03-20 16:10:40 +0000

    started admin home page #44

---
+++ Components/AdminHome.svelte
@@ -0,0 +1,8 @@
+<script>
+    let maxClubsPrePage = $state(10)
+</script>
+<div class="admin-home">
+    <AdminSwitch enabled={true} value={adminPreviewType} onChange={(nextView) => adminPreviewType = nextView} />
+    <Header userType="admin" previewAs={adminPreviewType} />    
+    <h1>Admin Home</h1>
+</div>
