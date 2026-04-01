66bd0aa23340b882e202c4eb375a70d49fb3d61c
Ben McKallip <mckallipb@southwestern.edu>
2026-04-01 15:16:33 +0000

    add new users to dev group

--- user_setup.sh
+++ user_setup.sh
@@ -5,6 +5,7 @@ do
     echo -e "ubuntu\nubuntu" | passwd $user
     sudo passwd -e $user
     usermod -aG sudo $user
+    usermod -aG devs $user
     mkdir -p /home/$user/
     ln -s /mnt/capstone /home/$user/
 done
